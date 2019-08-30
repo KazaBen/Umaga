4b195db9c03968fd2c19d29a97a477ec257716c9
Ben <kazabenas@gmail.com>
2019-08-30 03:10:11 +0300

    little cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -75,38 +75,22 @@ class App extends Component {
         });
     };
 
-    // our delete method that uses our backend api
-    // to remove existing database information
-    deleteFromDB = (idTodelete) => {
-        parseInt(idTodelete);
-        let objIdToDelete = null;
-        this.state.data.forEach((dat) => {
-            if (dat.id == idTodelete) {
-                objIdToDelete = dat._id;
-            }
-        });
-
-        axios.delete('http://localhost:3001/api/deleteData', {
-            data: {
-                id: objIdToDelete,
-            },
-        });
-    };
-
     // our update method that uses our backend api
     // to overwrite existing data base information
-    updateDB = (idToUpdate, updateToApply) => {
-        let objIdToUpdate = null;
-        parseInt(idToUpdate);
-        this.state.data.forEach((dat) => {
-            if (dat.id == idToUpdate) {
-                objIdToUpdate = dat._id;
-            }
-        });
+    updateDB = (idChamp, newName, newArmor,
+    newAttackDamage, newMana, newHp) => {
 
-        axios.post('http://localhost:3001/api/updateData', {
-            id: objIdToUpdate,
-            update: { message: updateToApply },
+
+        axios.post('http://localhost:3001/api/updateChamp', {
+            idChamp: idChamp,
+            champ: {
+                name: newName,
+                armor: parseInt(newArmor),
+                attackDamage: parseInt(newAttackDamage),
+                mana: parseInt(newMana),
+                hp: this.state.newHp
+            }
+
         });
     };
 
@@ -131,44 +115,48 @@ class App extends Component {
                 <div style={{ padding: '10px' }}>
                     <input
                         type="text"
-                        onChange={(e) => this.setState({ message: e.target.value })}
-                        placeholder="add something in the database"
                         style={{ width: '200px' }}
-                    />
-                    <button onClick={() => this.putDataToDB(this.state.message)}>
-                        ADD
-                    </button>
-                </div>
-                <div style={{ padding: '10px' }}>
-                    <input
-                        type="text"
-                        style={{ width: '200px' }}
-                        onChange={(e) => this.setState({ idToDelete: e.target.value })}
-                        placeholder="put id of item to delete here"
-                    />
-                    <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-                        DELETE
-                    </button>
-                </div>
-                <div style={{ padding: '10px' }}>
-                    <input
-                        type="text"
-                        style={{ width: '200px' }}
-                        onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-                        placeholder="id of item to update here"
+                        onChange={(e) => this.setState({ idChamp: e.target.value })}
+                        placeholder="id of the champion"
                     />
                     <input
                         type="text"
                         style={{ width: '200px' }}
-                        onChange={(e) => this.setState({ updateToApply: e.target.value })}
-                        placeholder="put new value of the item here"
+                        onChange={(e) => this.setState({ newName: e.target.value })}
+                        placeholder="new name"
                     />
+                    <input
+                        type="text"
+                        style={{ width: '200px' }}
+                        onChange={(e) => this.setState({ newArmor: e.target.value })}
+                        placeholder="new armor"
+                    />
+                    <input
+                        type="text"
+                        style={{ width: '200px' }}
+                        onChange={(e) => this.setState({ newAttackDamage: e.target.value })}
+                        placeholder="new attack damage"
+                    />
+                    <input
+                        type="text"
+                        style={{ width: '200px' }}
+                        onChange={(e) => this.setState({ newMana: e.target.value })}
+                        placeholder="new mana"
+                    />
+                    <input
+                        type="text"
+                        style={{ width: '200px' }}
+                        onChange={(e) => this.setState({ newHp: e.target.value })}
+                        placeholder="new hp"
+                    />
+
                     <button
                         onClick={() =>
-                            this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+                            this.updateDB(this.state.idChamp, this.state.newName, this.state.newArmor,
+                                this.state.newAttackDamage, this.state.newMana, this.state.newHp)
                         }
                     >
-                        UPDATE
+                        UPDATE CHAMP
                     </button>
                     <button
                         onClick={() =>
@@ -216,7 +204,12 @@ class App extends Component {
                     </button>
                 </div>
 
+
+
+
             </div>
+
+
         );
     }
 }
